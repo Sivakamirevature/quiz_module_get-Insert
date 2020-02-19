6ce474220efaf2f7572f0dd20c555c87f2d7ccb8
Sivakamirevature <60508731+Sivakamirevature@users.noreply.github.com>
2020-02-19 19:47:11 +0530

    Update QuizServiceImp.java

--- src/main/java/com/example/demo/service/QuizServiceImp.java
+++ src/main/java/com/example/demo/service/QuizServiceImp.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.ExceptionsHandling.DBExceptions;
+import com.example.demo.ExceptionsHandling.ServiceExceptions;
 import com.example.demo.dao.QuizDao;
 import com.example.demo.model.Quiz;
 
@@ -13,19 +14,42 @@ import com.example.demo.model.Quiz;
 public class QuizServiceImp implements QuizService {
 	@Autowired
 	QuizDao quizdao;
-
+	@Autowired
+	Quiz q;
 	@Override
-	public List<Quiz> getAllQuizzes() throws DBExceptions{
+	public List<Quiz> getAllQuizzes() throws ServiceExceptions, DBExceptions{
 		return quizdao.getAllQuizzes();
 	}
 
 	@Override
-	public List<Quiz> getQuizByID(int id)throws DBExceptions {
+	public List<Quiz> getQuizByID(int id)throws ServiceExceptions, DBExceptions{
+		ServiceExceptions serviceExceptions = new ServiceExceptions();
+		try{
+			
+			}
+		catch(IllegalArgumentException e) {
+			serviceExceptions.ArgumentMismatching("ID is not valid",e);
+		}
+		catch(Exception e){
+			throw serviceExceptions;
+		}
 		return quizdao.getQuizByID(id);
+	}
+	@Override
+	public Quiz createQuiz(Quiz quiz) throws ServiceExceptions,DBExceptions{
+		//System.out.println("from Service " +quiz.getCategory().getCategoryId());
+		Quiz q = quizdao.createQuiz(quiz);  
+		return q;
+	}
+	@Override
+	public int DeleteAllQuizzes() {
+		int id = quizdao.DeleteAllQuizzes();
+		return id;
 	}
 
 	@Override
-	public String createQuiz(Quiz quiz) throws DBExceptions {
-		return quizdao.createQuiz(quiz);
+	public int DeleteById(int qid) throws ServiceExceptions, DBExceptions {
+		return quizdao.deleteById(qid);
+		
 	}
 }

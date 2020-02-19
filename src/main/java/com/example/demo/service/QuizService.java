ce8d68ece00dd471e350e6ce719864824fab751c
Sivakamirevature <60508731+Sivakamirevature@users.noreply.github.com>
2020-02-19 19:46:36 +0530

    Update QuizService.java

--- src/main/java/com/example/demo/service/QuizService.java
+++ src/main/java/com/example/demo/service/QuizService.java
@@ -2,11 +2,17 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.demo.ExceptionsHandling.DBExceptions;
+import com.example.demo.ExceptionsHandling.ServiceExceptions;
 import com.example.demo.model.Quiz;
 
 public interface QuizService {
-	List<Quiz> getAllQuizzes()throws DBExceptions;
-	List<Quiz> getQuizByID(int id)throws DBExceptions;
-	String createQuiz(Quiz quiz) throws DBExceptions;
+	
+	List<Quiz> getAllQuizzes()throws ServiceExceptions,DBExceptions;
+	List<Quiz> getQuizByID(int id)throws ServiceExceptions,DBExceptions;
+	Quiz createQuiz(Quiz quiz) throws ServiceExceptions,DBExceptions;
+	int DeleteAllQuizzes();
+	int DeleteById(int qid)throws ServiceExceptions,DBExceptions;
 }
